a3c18e50a8963fa12ddda1bcac3baf20f61206ef
PATEL MOHIT <mohitptl183@gmail.com>
2024-08-10 23:06:36 +0530

    Add Atlas

--- app.js
+++ app.js
@@ -31,15 +31,20 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 60000,
+  socketTimeoutMS: 45000,
+  poolSize: 10,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
 
 app.use('/api', authRouter);
 app.use('/api/images', imageRouter);
 app.use('/api/v1/user', userRout);
 app.use('/api/v1/senseistu', senseisStuRouter);
-// app.use('/register', ownersRouter);
 
 
 app.get('/', (req, res) => {
