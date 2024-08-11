bb1e6bae17571838dd9e93a98d0b49f5f88a41bf
PATEL MOHIT <mohitptl183@gmail.com>
2024-08-11 19:02:06 +0530

    Updated CORS origin

--- routes/senseisStuRouter.js
+++ routes/senseisStuRouter.js
@@ -12,10 +12,10 @@ const {
 const uploadFields = require("../config/multer-config");
 const authenticateToken = require("../middlewarse/authenticateToken");
 
-router.post("/register", authenticateToken, uploadFields, registerStu);
+router.post("/register", uploadFields, registerStu);
 router.get("/get", getStudent);
 router.get("/getAllStu", getAllStu);
-router.get("/get/:id", authenticateToken, getStudentById);
+router.get("/get/:id", getStudentById);
 router.get("/get/student/:id", getAllStuBySensei);
 router.put("/update/:id", updateStudent);
 
